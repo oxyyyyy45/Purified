a6aaa710aa6a8d316a67814606b61f974f4bd9ec
Gravity <wooddominic46@gmail.com>
2026-08-20 16:35:50 -0500

    Update firstmsg.js

--- src/commands/Utility/firstmsg.js
+++ src/commands/Utility/firstmsg.js
@@ -1,61 +1,62 @@
-import { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField, ChannelType } from 'discord.js';
 import { createEmbed, errorEmbed, successEmbed } from '../../utils/embeds.js';
-import { InteractionHelper } from '../../utils/interactionHelper.js';
 import { logger } from '../../utils/logger.js';
+
 export default {
-    data: new SlashCommandBuilder()
-        .setName("firstmsg")
-        .setDescription("Get a link to the first message in this channel")
-        .setDMPermission(false)
-        .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+    name: "firstmsg",
+    description: "Get a link to the first message in this channel",
     category: "Utility",
 
-    async execute(interaction, config, client) {
-        const deferSuccess = await InteractionHelper.safeDefer(interaction);
-        if (!deferSuccess) {
-            logger.warn(`FirstMsg interaction defer failed`, {
-                userId: interaction.user.id,
-                guildId: interaction.guildId,
-                commandName: 'firstmsg'
-            });
-            return;
-        }
+    async execute(message, args, client, config) {
+        // 1. Guard for DM channels (since .setDMPermission(false) was used)
+        if (!message.guild) return;
 
-        const messages = await interaction.channel.messages.fetch({
+        // 2. Fetch the oldest message in the channel
+        const messages = await message.channel.messages.fetch({
             limit: 1,
             after: '1',
             cache: false
+        }).catch((err) => {
+            logger.error(`FirstMsg - failed to fetch messages`, {
+                userId: message.author.id,
+                channelId: message.channel.id,
+                error: err.message
+            });
+            return null;
         });
 
-        const firstMessage = messages.first();
+        const firstMessage = messages?.first();
 
+        // 3. Handle empty channels
         if (!firstMessage) {
             logger.info(`FirstMsg - no messages found in channel`, {
-                userId: interaction.user.id,
-                channelId: interaction.channelId,
-                guildId: interaction.guildId
+                userId: message.author.id,
+                channelId: message.channel.id,
+                guildId: message.guild.id
             });
-            return await InteractionHelper.safeEditReply(interaction, {
+            return await message.reply({
                 embeds: [successEmbed('First Message', "No messages found in this channel!")],
             });
         }
 
-        const messageLink = `https://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${firstMessage.id}`;
+        // 4. Generate the direct message link
+        const messageLink = `https://discord.com/channels/${message.guild.id}/${message.channel.id}/${firstMessage.id}`;
 
-        await InteractionHelper.safeEditReply(interaction, {
+        // 5. Send the reply
+        await message.reply({
             embeds: [
                 successEmbed(
-                    "First Message in #" + interaction.channel.name,
+                    "First Message in #" + message.channel.name,
                     `Message Link: ${messageLink}`
                 ),
             ],
         });
 
+        // 6. Log completion
         logger.info(`FirstMsg command executed`, {
-            userId: interaction.user.id,
-            channelId: interaction.channelId,
+            userId: message.author.id,
+            channelId: message.channel.id,
             messageId: firstMessage.id,
-            guildId: interaction.guildId
+            guildId: message.guild.id
         });
     },
 };
